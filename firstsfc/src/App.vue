57f91f7c1b64732d3fce4b2bf18891fbf794640f
Zaina Cassandra S. Villanueva <zsvillanueva@student.apc.edu.ph>
2026-02-19 13:05:34 +0800

    edited

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -16,12 +16,4 @@ export default {
 };
 </script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  </style>
+<style></style>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,5 +6,5 @@ import FoodItem2 from './components/FoodItem2.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item', FoodItem2)
+app.component('food-item2', FoodItem2)
 app.mount('#app')
