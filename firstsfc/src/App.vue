f769f40ab6e0cb780e80a73d07b22676d25cdf8f
Zaina Cassandra S. Villanueva <zsvillanueva@student.apc.edu.ph>
2026-02-27 12:46:41 +0000

    TRY

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from '/src/lib/supabaseClient.js'
+import { supabase } from 'src/lib/supabaseClient.js'
+
 const instruments = ref([])
+
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
   instruments.value = data
 }
+
 onMounted(() => {
    getInstruments()
 })
 </script>
+
 <template>
   <ul>
     <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
